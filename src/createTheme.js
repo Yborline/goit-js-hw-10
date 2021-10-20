82356577f10ece7071850d9e6a0c86662e596f22
Yaroslav Borys <borrline@gmail.com>
2021-10-21 02:27:28 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/createTheme.js
@@ -0,0 +1,32 @@
+//  export default function toggleTheme() {
+
+//      if (document.body.classList.contains('light-theme')) {
+//          createDarkTheme()
+//          localStorage.setItem('createTheme', 'dark-theme');
+
+
+//     } else {
+//          createLightTheme();
+//          localStorage.setItem('createTheme', 'light-theme')
+//     }
+// }
+
+// function createTheme(evt) {
+//     localStorage.setItem('createTheme', evt)
+
+// }
+
+
+// function createDarkTheme() {
+//        document.body.classList.remove('light-theme');
+//     document.body.classList.add('dark-theme');
+ 
+
+       
+// }
+
+// function createLightTheme() {
+//      document.body.classList.remove('dark-theme')    
+//             document.body.classList.add('light-theme')
+
+// }
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import cardsFood from "./menu.json"
+
+// import toggleTheme from "./createTheme"
+import createMurkupList from "./createMurkup"
+
+
+const listFoodEl = document.querySelector('.js-menu');
+
+
+const createMurkup = createMurkupList(cardsFood);
+listFoodEl.insertAdjacentHTML('afterbegin', createMurkup);
+
+const savedTheme = localStorage.getItem('createTheme');
+
+document.body.classList.add(savedTheme);
+
+const toggleSwitchTheme = document.querySelector('#theme-switch-toggle')
+
+
+
+
+toggleSwitchTheme.addEventListener('change', (evt) => {
+
+     if (evt.target.checked) {
+         createDarkTheme('dark-theme','light-theme')
+         createTheme('dark-theme');
+
+    } else {
+          createDarkTheme('light-theme','dark-theme')
+     
+            createTheme('light-theme');
+    }
+}
+)
+
+function createTheme(evt) {
+    localStorage.setItem('createTheme', evt)
+
+}
+
+function createDarkTheme(add , remove) {
+    document.body.classList.remove(remove);'light-theme'
+    document.body.classList.add(add);'dark-theme'
+
+       
+}
+
+function createLightTheme() {
+     document.body.classList.remove('dark-theme')    
+            document.body.classList.add('light-theme')
+
+}
+// сonsole.log(document.body.className);
